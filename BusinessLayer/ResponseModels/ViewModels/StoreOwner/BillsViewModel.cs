833a92100e79af9cc582cd3eaf8d729c14292832
Biscottezi <nguyenphamminhquan7516@gmail.com>
2022-02-09 07:52:05 +0700

    store-owner get bills ând get bill by id

---
+++ BusinessLayer/ResponseModels/ViewModels/StoreOwner/BillsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAcessLayer.Models;
+
+namespace BusinessLayer.ResponseModels.ViewModels.StoreOwner
+{
+    public class BillsViewModel
+    {
+        public BillsViewModel()
+        {
+            BillDetails = new HashSet<BillDetail>();
+        }
+
+        public int Id { get; set; }
+        public int StoreId { get; set; }
+        public int CashierId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int TotalPrice { get; set; }
+
+        public virtual DataAcessLayer.Models.Cashier Cashier { get; set; }
+        public virtual Store Store { get; set; }
+        public virtual ICollection<BillDetail> BillDetails { get; set; }
+    }
+}
